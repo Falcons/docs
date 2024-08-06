82a402b58a582433117763390ae8c4dedc26b713
eGuitar999 <ari.vancruyningen@gmail.com>
2024-08-05 21:45:30 -0400

    spacing fix and colour test

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       },
       backgroundImage: {
         index: "linear-gradient(to top, rgba(60, 60, 60, 1), rgba(243, 244, 246, 0.5)), url(/assets/images/robot_close.png)",
-        translucent: "linear-gradient(to top, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.2))"
+        translucent: "linear-gradient(to top, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1))"
       }
     },
   },
